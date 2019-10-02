5dec602d24f6c4143b3a41b63ca809a0325b8643
Shankhadeep <shankhadeepdey99@gmail.com>
2019-10-02 14:54:51 +0530

    Added solution of 2017 day 2

---
+++ 2017/Day 2-2.py
@@ -0,0 +1,19 @@
+import string
+import itertools
+#a Python2 solution
+lines = []
+with open('Day 2 - input') as fp:
+	for line in fp:
+		lines.append(map(int, string.split(line.strip(), '\t')))
+
+part1 = 0
+part2 = 0
+for line in lines:
+	part1 += max(line) - min(line)
+
+	for i in itertools.combinations(line, 2):
+		if max(i) % min(i) == 0:
+			part2 += max(i) / min(i)
+			break
+
+print ('Part 2: %s' % part2)
