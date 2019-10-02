50337bf7e658f6a7ed24d9dae7a21d77e65afc0f
Carlos Sánchez Páez <karlossanpa@gmail.com>
2019-10-02 12:30:10 +0200

    Used itertools.combinations

--- 2017/Day 2-2.py
+++ 2017/Day 2-2.py
@@ -1,15 +1,19 @@
+from itertools import combinations
 checksum = 0
 with open("Day 2 - input", "r") as file:
     for line in file:
         row = [int(i) for i in line.split('\t')]
-        for i in range(0,len(row)):
-            for second in row[i+1:]:
-                first = row[i]
-                result = 0
-                if  (first/second).is_integer():
-                    result = first/second
-                elif (second/first).is_integer():
-                    result = second/first
-                checksum = checksum + result
+        combs = list(combinations(row,2))
+        for combination in combs:
+            result = 0
+            first = combination[0]/combination[1]
+            second = combination[1]/combination[0]
+            if  first.is_integer():
+                result = first
+            elif second.is_integer():
+                result = second
+            checksum = checksum + result
+            if result != 0:
+                break
 
 print("The checksum is {}".format(checksum))
