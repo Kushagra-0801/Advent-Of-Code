b469b7616e1c5490a202a5abd717ce19c1a7833a
Caio <kaioesperto@hotmail.com>
2019-10-02 11:14:22 +0100

    Update Day 1.py

--- 2017/Day 1.py
+++ 2017/Day 1.py
@@ -1,13 +1,20 @@
-input = 91212129
+f = open("input.txt", 'r')
+input = f.read()
 
 arr = list(map(int, list(str(input))))
+range_match = int(len(arr) / 2)
 res = 0
-for i in range(0, len(arr)-1):
-	# if its the last number then compare with the first one
-	if(i+1 == len(arr)-1):
-		if(arr[i+1] == arr[0]):
-			res += arr[i+1]
-	else:
-		if(arr[i] == arr[i+1]):
+
+for i in range(0, len(arr)):
+	if(i + range_match > len(arr)):
+		remain_amount = (range_match + i) - len(arr)
+		if(arr[i] == arr[remain_amount]):
 			res += arr[i]
+	elif(i+range_match == len(arr)):
+		if(arr[i] == arr[0]):
+			res += arr[i]
+	else:
+		if(arr[i] == arr[i+range_match]):
+			res += arr[i]
+
 print(res)
