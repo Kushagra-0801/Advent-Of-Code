9d4d97dcacc0ab6bb635bfac6ee28ceca6623f13
Exp <theexp111@gmail.com>
2019-10-03 14:16:53 +0200

    snake_case

--- 2016/Day 4-1.py
+++ 2016/Day 4-1.py
@@ -1,21 +1,21 @@
 from collections import Counter
 from functools import cmp_to_key
 
-def extractFile(file):
-    return [line[:-1] for line in file]
+def extract_file(file):
+    return [line.strip() for line in file]
 
-def sortComp(a, b): # parameters are tuples (letter, count)
+def sort_comp(a, b): # parameters are tuples (letter, count)
     return ord(a[0]) - ord(b[0]) if a[1] == b[1] else b[1] - a[1] 
 
-def mostCommon(line, n):
+def most_common(line, n):
     letters = Counter(line).most_common()
     letters = [letter for letter in letters if letter[0].isalpha()]
-    letters = sorted(letters, key=cmp_to_key(sortComp))
+    letters = sorted(letters, key=cmp_to_key(sort_comp))
     return [letter for letter,_ in letters[:5]]
 
-def validRoom(line):
-    return "[{0}]".format(''.join(mostCommon(line[:-7], 5))) == line[-7:]
+def valid_room(line):
+    return "[{0}]".format(''.join(most_common(line[:-7], 5))) == line[-7:]
 
 with open('Day 4-Input') as file:
-    lines = extractFile(file)
-    print(sum(int(line[-10:-7]) for line in lines if validRoom(line)))
+    lines = extract_file(file)
+    print(sum(int(line[-10:-7]) for line in lines if valid_room(line)))
--- 2016/Day 4-2.py
+++ 2016/Day 4-2.py
@@ -1,22 +1,22 @@
 from collections import Counter
 from functools import cmp_to_key
 
-def extractFile(file):
-    return [line[:-1] for line in file]
+def extract_file(file):
+    return [line.strip() for line in file]
 
-def sortComp(a, b): # parameters are tuples (letter, count)
+def sort_comp(a, b): # parameters are tuples (letter, count)
     return ord(a[0]) - ord(b[0]) if a[1] == b[1] else b[1] - a[1] 
 
-def mostCommon(line, n):
+def most_common(line, n):
     letters = Counter(line).most_common()
     letters = [letter for letter in letters if letter[0].isalpha()]
-    letters = sorted(letters, key=cmp_to_key(sortComp))
+    letters = sorted(letters, key=cmp_to_key(sort_comp))
     return [letter for letter,_ in letters[:5]]
 
-def validRoom(line):
-    return "[{0}]".format(''.join(mostCommon(line[:-7], 5))) == line[-7:]
+def valid_room(line):
+    return "[{0}]".format(''.join(most_common(line[:-7], 5))) == line[-7:]
 
-def rotN(s, n):
+def rot_n(s, n):
     def lookup(v, n):
         o = ord(v)
         n = n % 26
@@ -28,9 +28,9 @@ def rotN(s, n):
     return ''.join([lookup(char, n) for char in s])
 
 def decrypt(room): # Rotate by sectorID
-    return rotN(room[:-11], int(room[-10:-7]))
+    return rot_n(room[:-11], int(room[-10:-7]))
 
 with open('Day 4-Input') as file:
-    lines = extractFile(file)
-    lines = [line for line in lines if validRoom(line)]
+    lines = extract_file(file)
+    lines = [line for line in lines if valid_room(line)]
     [print(line[-10:-7]) for line in lines if "north" in decrypt(line)]
