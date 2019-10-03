ece40941854b073c76fa80b20d23762c8a477ef1
Exp <theexp111@gmail.com>
2019-10-03 14:19:10 +0200

    snake_case

--- 2016/Day 5-2.py
+++ 2016/Day 5-2.py
@@ -1,10 +1,10 @@
 from hashlib import md5
 import itertools
 
-def extractFile(file):
+def extract_file(file):
     return file.readline()
 
-def prettyPrint(pw, n):
+def pretty_print(pw, n):
     s = "________"
     for i in range(n):
         if pw[i]:
@@ -12,19 +12,19 @@ def prettyPrint(pw, n):
     print(s)
 
 with open('Day 5-Input') as file:
-    line = extractFile(file)
+    line = extract_file(file)
     n = 8
     pw = [None] * n
     for i in itertools.count():
         cur = line + str(i)
-        md5Hash = md5(cur.encode()).hexdigest()
-        if md5Hash[:5] == "00000":
-            index = md5Hash[5:6]
+        md5_hash = md5(cur.encode()).hexdigest()
+        if md5_hash[:5] == "00000":
+            index = md5_hash[5:6]
             if index >= '0' and index < str(n):
                 index = int(index)
                 if not pw[index]: # not already written
-                    pw[index] = md5Hash[6:7]
-                    prettyPrint(pw, n)
+                    pw[index] = md5_hash[6:7]
+                    pretty_print(pw, n)
         if not None in pw:
             break
     print(''.join(pw))
