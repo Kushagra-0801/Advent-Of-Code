9d77cffc344d701a6a01d44e22e829fe701d79e4
Exp <theexp111@gmail.com>
2019-10-03 14:07:46 +0200

    snake_case & removed unnecessary things

--- 2016/Day 6-1.py
+++ 2016/Day 6-1.py
@@ -1,10 +1,9 @@
 from collections import Counter
 
-def extractFile(file):
-    return [line for line in file]
+def extract_file(file):
+    return [line.strip() for line in file]
 
 with open('Day 6-Input') as file:
-    lines = extractFile(file)
-    lines = [[char for char in line[:-1]] for line in lines]
-    lines = [Counter(line).most_common(1)[0][0] for line in list(zip(*lines))]
+    lines = extract_file(file)
+    lines = [Counter(line).most_common(1)[0][0] for line in zip(*lines)]
     print(''.join(lines))
