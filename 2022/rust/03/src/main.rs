e4eb2b2b1dcac03cd59e40518ce526fb845d8639
Kushagra Gupta <kushgpt08@gmail.com>
2022-12-04 20:43:33 +0530

    part 1

---
+++ 2022/rust/03/src/main.rs
@@ -0,0 +1,86 @@
+use std::{collections::HashSet, error::Error};
+
+static INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../../inputs/03"));
+
+type AocResult<T> = Result<T, Box<dyn Error>>;
+type Int = u64;
+
+fn get_priority(item: char) -> AocResult<Int> {
+    match item {
+        'a'..='z' => Ok((item as u8 - b'a' + 1).into()),
+        'A'..='Z' => Ok((item as u8 - b'A' + 27).into()),
+        _ => Err(format!("Invalid rucksack item: {item:?}").into()),
+    }
+}
+
+fn part1(input: &str) -> AocResult<Int> {
+    let mut total_priority = 0;
+    let mut scratch = HashSet::with_capacity(52 / 2 + 1); // One partition will have half the items plus one repeat
+    for rucksack in input.trim().lines() {
+        if rucksack.is_empty() || rucksack.len() % 2 != 0 || !rucksack.is_ascii() {
+            return Err(format!("Invalid rucksack configuration: {rucksack:?}").into());
+        }
+        let mid_pt = rucksack.len() / 2;
+        let (compartment1, compartment2) = rucksack.split_at(mid_pt);
+        scratch.clear();
+        compartment1.chars().for_each(|c| {
+            scratch.insert(c);
+        });
+        let repeat_item = compartment2
+            .chars()
+            .find(|c| scratch.contains(c))
+            .ok_or_else(|| format!("No repeat item in rucksack: {rucksack:?}"))?;
+        total_priority += get_priority(repeat_item)?;
+    }
+    Ok(total_priority)
+}
+
+fn main() -> AocResult<()> {
+    let part1 = part1(INPUT)?;
+    println!("Part 1: {part1}");
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    use super::*;
+
+    struct TestCase {
+        input: &'static str,
+        output1: &'static str,
+        output2: &'static str,
+    }
+
+    impl From<(&'static str, &'static str, &'static str)> for TestCase {
+        fn from((i, o1, o2): (&'static str, &'static str, &'static str)) -> Self {
+            Self {
+                input: i,
+                output1: o1,
+                output2: o2,
+            }
+        }
+    }
+
+    static TEST_CASES: [(&str, &str, &str); 1] = [(
+        indoc! {"
+            vJrwpWtwJgWrhcsFMMfFFhFp
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+            PmmdzqPrVvPwwTWBwg
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+            ttgJtRGJQctTZtZT
+            CrZsJsPPZsGzwwsLwLmpwMDw
+        "},
+        "157",
+        "",
+    )];
+
+    #[test]
+    fn part1_test() {
+        for case in TEST_CASES {
+            let case = TestCase::from(case);
+            assert_eq!(part1(case.input).unwrap().to_string(), case.output1);
+        }
+    }
+}
