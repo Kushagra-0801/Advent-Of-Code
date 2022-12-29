d709728879cf46bab076242c02ae5d4f1197acb1
Kushagra Gupta <kushgpt08@gmail.com>
2022-12-29 23:10:02 +0530

    solve part 1 - without iterators

---
+++ 2022/rust/08/src/main.rs
@@ -0,0 +1,108 @@
+use anyhow::Result;
+use aoc_parse::{parser, prelude::*};
+use aoc_utils::grids::{Grid, Idx};
+
+static INPUT: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../../inputs/08"));
+
+type TreeCover = Grid<u8>;
+type Input = TreeCover;
+
+fn parse(input: &'static str) -> Result<Input> {
+    let p = parser!(lines({h:digit => h as u8}+));
+    let grid = p
+        .parse(input)
+        .map(|grid| grid.into_iter().flatten().collect())?;
+    let columns = input
+        .lines()
+        .next()
+        .map(str::len)
+        .expect("The parser worked above so input cannot have been empty");
+    let grid = Grid::try_from_vec(grid, columns)?;
+    Ok(grid)
+}
+
+fn is_tree_visible(tree: Idx, grid: &TreeCover) -> bool {
+    let tree_height = grid[tree];
+
+    let col_indexer = |col| Idx { row: tree.row, col };
+    let trees_in_row = [
+        (0..tree.col).map(col_indexer),                // left
+        (tree.col + 1..grid.columns).map(col_indexer), // right
+    ];
+    let row_indexer = |row| Idx { row, col: tree.col };
+    let trees_in_col = [
+        (0..tree.row).map(row_indexer),             // up
+        (tree.row + 1..grid.rows).map(row_indexer), // down
+    ];
+
+    trees_in_row
+        .into_iter()
+        .any(|mut t| t.all(|x| grid[x] < tree_height))
+        || trees_in_col
+            .into_iter()
+            .any(|mut t| t.all(|x| grid[x] < tree_height))
+}
+
+fn part1(tree_cover: Input) -> Result<String> {
+    let mut visible_trees = 0;
+    for row in 0..(tree_cover.rows) {
+        for col in 0..(tree_cover.columns) {
+            visible_trees += is_tree_visible(Idx { row, col }, &tree_cover) as usize;
+        }
+    }
+    Ok(visible_trees.to_string())
+}
+
+fn part2(input: Input) -> Result<String> {
+    todo!()
+}
+
+fn main() -> Result<()> {
+    let input = parse(INPUT)?;
+    let part1 = part1(input.clone())?;
+    println!("part1: {part1}");
+    let part2 = part2(input)?;
+    println!("part2: {part2}");
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    use aoc_utils::TestCase;
+
+    use super::*;
+
+    const TEST_CASES: &[[&str; 3]] = &[[
+        indoc! {"
+            30373
+            25512
+            65332
+            33549
+            35390
+        "},
+        "21",
+        "",
+    ]];
+
+    #[test]
+    fn part1_test() {
+        for case in TEST_CASES {
+            let case = TestCase::from(case);
+            let input = parse(case.input).unwrap();
+            let output = part1(input).unwrap();
+            assert_eq!(output, case.out1, "Failed for {case:?}");
+        }
+    }
+
+    #[test]
+    fn part2_test() {
+        for case in TEST_CASES {
+            let case = TestCase::from(case);
+            let input = parse(case.input).unwrap();
+            let output = part2(input).unwrap();
+            assert_eq!(output, case.out2, "Failed for {case:?}");
+        }
+    }
+}
--- utils/rust/aoc-utils/src/lib.rs
+++ utils/rust/aoc-utils/src/lib.rs
@@ -1,3 +1,77 @@
+pub mod grids {
+    use std::ops::{Index, IndexMut};
+
+    use anyhow::{ensure, Result};
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub struct Idx {
+        pub row: usize,
+        pub col: usize,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq, Default)]
+    pub struct Grid<T> {
+        pub grid: Vec<T>,
+        pub columns: usize,
+        pub rows: usize,
+    }
+
+    impl<T> Grid<T> {
+        pub fn with_capacity(rows: usize, cols: usize) -> Self {
+            Self {
+                grid: Vec::with_capacity(rows * cols),
+                columns: 0,
+                rows: 0,
+            }
+        }
+
+        pub fn try_from_vec(grid: Vec<T>, columns: usize) -> Result<Self> {
+            ensure!(
+                grid.len() % columns == 0,
+                "Provided vector can not be fit into a rectangular grid"
+            );
+            let rows = grid.len() / columns;
+            Ok(Self {
+                grid,
+                columns,
+                rows,
+            })
+        }
+    }
+
+    impl<T> Index<usize> for Grid<T> {
+        type Output = [T];
+
+        fn index(&self, index: usize) -> &Self::Output {
+            let start = index * self.columns;
+            let end = start + self.columns;
+            &self.grid[start..end]
+        }
+    }
+
+    impl<T> IndexMut<usize> for Grid<T> {
+        fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+            let start = index * self.columns;
+            let end = start + self.columns;
+            &mut self.grid[start..end]
+        }
+    }
+
+    impl<T> Index<Idx> for Grid<T> {
+        type Output = T;
+
+        fn index(&self, index: Idx) -> &Self::Output {
+            &self[index.row][index.col]
+        }
+    }
+
+    impl<T> IndexMut<Idx> for Grid<T> {
+        fn index_mut(&mut self, index: Idx) -> &mut Self::Output {
+            &mut self[index.row][index.col]
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct TestCase {
     pub input: &'static str,
